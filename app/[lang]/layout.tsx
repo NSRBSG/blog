443221e8fab1a494554f7729f3bc47b5ae6df2d7
NSRBSG <nsrbsg@gmail.com>
2025-09-30 17:13:45 +0900

    style: fix font

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,8 +1,14 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Nanum_Gothic } from 'next/font/google';
 import { Locale } from '@/lib/i18n/config';
+import Header from '../components/header';
+import '../globals.css';
+import { ThemeProvider } from '../components/theme-provider';
 
-const inter = Inter({ subsets: ['latin'] });
+const nanumGothic = Nanum_Gothic({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
 
 export const metadata: Metadata = {
   title: 'Blog',
@@ -18,8 +24,13 @@ export default async function RootLayout({
 }>) {
   const { lang } = await params;
   return (
-    <html lang={lang}>
-      <body className={inter.className}>{children}</body>
+    <html lang={lang} suppressHydrationWarning>
+      <body className={nanumGothic.className}>
+        <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+          <Header lang={lang} />
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
