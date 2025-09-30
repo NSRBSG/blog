2993e3c301d4a2a75caaa1f280723b3be9adcf76
NSRBSG <nsrbsg@gmail.com>
2025-09-30 17:14:18 +0900

    chore: add static variable

--- lib/i18n/config.ts
+++ lib/i18n/config.ts
@@ -2,4 +2,9 @@ export const locales = ['ko', 'en'] as const;
 
 export type Locale = (typeof locales)[number];
 
+export const localeToLabel: Record<Locale, string> = {
+  ko: '한국어',
+  en: 'English',
+};
+
 export const defaultLocale: Locale = 'ko';
